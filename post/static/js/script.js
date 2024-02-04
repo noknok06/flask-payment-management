db6ed30d9129db781ea103e759139e0f12a4ba11
Naoki0618 <nok.h0618@gmail.com>
2024-02-04 11:03:49 +0900

    update

---
+++ post/static/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#data-table').DataTable({
+        // 日本語表示
+        "language": {
+            "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json",
+            
+        }
+    });
+});
