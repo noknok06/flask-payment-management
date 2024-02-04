70187cfdc2a971068b40a33fe1e200164f0a28be
Naoki0618 <nok.h0618@gmail.com>
2024-02-04 21:22:19 +0900

    管理画面変更

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Post, Company
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("contract_partner", "category", "title",
+                    "contract_method", "recording_date")  # タイトルと著者を表示するよう設定
+    list_filter = ("contract_partner",) # authorでフィルター
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Company)
